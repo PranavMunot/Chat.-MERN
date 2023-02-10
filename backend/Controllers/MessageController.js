3e36139174309fe89d36e0b5b21e7e261c2b13e8
PranavMunot <pranavmunot09@gmail.com>
2023-02-10 10:07:59 +0530

    v0.0.2

--- backend/Controllers/MessageController.js
+++ backend/Controllers/MessageController.js
@@ -26,7 +26,10 @@ exports.getMessages = async (req, res, next) => {
     const from = req.user._id
     const { to } = req.body
 
-    const foundMessages = await Message.find({ from: from, to: to }).sort({ createdAt: -1 }).limit(3)
+    // const foundMessages = await Message.find({ from: from, to: to }).sort({ createdAt: -1 }).limit(3).sort({ createdAt: 1 }).exec()
+    const foundMessages = await Message.aggregate([
+        { $match: { from: from, to: to } }
+    ])
 
     res.status(200).json({
         success: true,
