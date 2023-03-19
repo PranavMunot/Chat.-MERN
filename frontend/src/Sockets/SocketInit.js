3e20b26ce223c6daa270d658a580b957a1a4f1cc
PranavMunot <pranavmunot09@gmail.com>
2023-03-20 01:33:02 +0530

    v0.0.1

--- frontend/src/Sockets/SocketInit.js
+++ frontend/src/Sockets/SocketInit.js
@@ -1,7 +1,7 @@
 // sockets are rockets
 import io from 'socket.io-client'
 
-const socket = io.connect('http://localhost:4000', {
+const socket = io.connect('https://chatbackend-74wf.onrender.com', {
     reconnectionAttempts: 5, // Maximum number of reconnection attempts
     reconnectionDelay: 1000, // Delay between each attempt (in milliseconds)
 })
